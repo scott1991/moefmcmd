3d5a53f2503604e37e3e2fae0db25e6eff70fd43
scott1991 <cakepbm@gmail.com>
2015-07-21 16:27:47 +0800

    fix play problem

--- moefmcmd.sh
+++ moefmcmd.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-# 脚本名:
-#   萌否电台客户端bash脚本
-# 依赖软件:
-#   mpg123, jq, curl
-# 历史:
-#   2013/10/12  Mike Akiba  初次发布
 
-#get_moefm_json () {
-#    curl -s -A moefmcmd.sh 'http://moe.fm/listen/playlist?api=json&api_key=4e229396346768a0c4ee2462d76eb0be052592bf8'
-#    return
-#}
 
 while true; do
     #number=$(curl -s -A moefmcmd.sh 'http://www.random.org/integers/?num=1&min=1&max=9&col=1&base=10&format=plain&rnd=new')
@@ -28,15 +18,16 @@ while true; do
     do
         mp3_url=$(echo $moefm_json | jq -M -r ".response.playlist[${a[$i]}].url")
         if [ "$mp3_url" == "null" ]; then
-            echo "url为null"
+            echo "url is null!"
             continue
         fi
         title=$(echo $moefm_json | jq -M -r ".response.playlist[${a[$i]}].sub_title")
         artist=$(echo $moefm_json | jq -M -r ".response.playlist[${a[$i]}].artist")
         album=$(echo $moefm_json | jq -M -r ".response.playlist[${a[$i]}].wiki_title")
         clear
-        printf '艺术家: %s\n曲名:   %s\n专辑:   %s\n\n[SPACE] 暂停/继续 [q] 下一曲 [Ctrl-Z] 退出\n' "$artist" "$title" "$album"
-        afplay -q  "$mp3_url"
+        printf ‘Artist: %s\nSong:   %s\nAlbum:   %s\n\n[SPACE] Pause/Continue [q] Next [Ctrl-Z] Exit\n’ "$artist" "$title" "$album"
+        wget -O "/tmp/mo.mp3" -q "$mp3_url"
+	afplay -v 0.2 -q 1 /tmp/mo.mp3
     done
 done
 
